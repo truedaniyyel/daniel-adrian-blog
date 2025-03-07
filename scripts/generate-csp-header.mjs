87e97668d8f36f8fbc9fca58f327f8e75378ba5e
off-origami <baskakovihor@gmail.com>
2025-03-07 11:27:54 -0800

    problems with cloudflare and sri

--- scripts/generate-csp-header.mjs
+++ scripts/generate-csp-header.mjs
@@ -10,7 +10,7 @@ import {
 
 const headersPath = path.join(process.cwd(), 'dist', '_headers');
 
-async function generateCSPHeaders() {
+async function generateCSPHeader() {
   try {
     // Combine all script hashes
     const scriptHashes = new Set([
@@ -26,43 +26,32 @@ async function generateCSPHeaders() {
       ...Object.values(perResourceSriHashes.styles),
     ]);
 
-    // Split CSP into multiple headers
-    const cspHeaders = [
-      // First CSP header with default-src, object-src, and script-src directives
+    // Generate CSP header
+    const cspHeader =
       `Content-Security-Policy: default-src 'self'; object-src 'self'; script-src 'self' ${Array.from(
         scriptHashes
       )
         .map(hash => `'${hash}'`)
-        .join(' ')};`,
-
-      // Second CSP header with connect-src, style-src, and base-uri directives
-      `Content-Security-Policy: connect-src 'self'; style-src 'self' ${Array.from(
+        .join(' ')}; connect-src 'self'; style-src 'self' ${Array.from(
         styleHashes
       )
         .map(hash => `'${hash}'`)
-        .join(' ')}; base-uri 'self';`,
-
-      // Third CSP header with remaining directives
-      `Content-Security-Policy: img-src 'self' https://ik.imagekit.io/truedaniyyel/; frame-ancestors 'none'; worker-src 'self'; manifest-src 'none'; form-action 'self';`,
-    ];
+        .join(
+          ' '
+        )}; base-uri 'self'; img-src 'self' https://ik.imagekit.io/truedaniyyel/; frame-ancestors 'none'; worker-src 'self'; manifest-src 'none'; form-action 'self';`.trim();
 
     // Read existing _headers file
     let headersContent = await fs.readFile(headersPath, 'utf-8');
 
-    // Add each CSP header on a new line
-    cspHeaders.forEach(header => {
-      headersContent += '\n  ' + header;
-    });
+    headersContent += '\n  ' + cspHeader;
 
     // Write updated content back to _headers file
     await fs.writeFile(headersPath, headersContent);
 
-    console.log(
-      'CSP headers generated and _headers file updated successfully.'
-    );
+    console.log('CSP header generated and _headers file updated successfully.');
   } catch (error) {
-    console.error('Error generating CSP headers:', error);
+    console.error('Error generating CSP header:', error);
   }
 }
 
-generateCSPHeaders();
+generateCSPHeader();
